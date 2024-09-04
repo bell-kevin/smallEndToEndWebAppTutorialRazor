7997798b2d40596a566c001ace867edcb5e3a24e
Kevin Bell <bellKevin@pm.me>
2024-09-04 15:59:10 -0600

    Add files via upload

---
+++ RazorPagesMovie/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
+
